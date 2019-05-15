7328045ec641fcc6dfb62aaeca7f8e14e4c3e2db
benjaminelizalde <benjaminelizalde24@gmail.com>
2019-05-15 10:25:32 -0700

    dhudshdksh

--- static/game.js
+++ static/game.js
@@ -1,24 +1,48 @@
-
 var cookies = 0;
+var cookiesPerSecond = 0;
 var cookiesPerClick = 1;
+
+
   $(document).ready(function() {
+    cookies = cookies + cookiesPerSecond;
+    $("#cookies").text(cookies);
+    $("#cookiesPerSecond").text(cookiesPerSecond);
+    $("#cookiesPerClick").text(cookiesPerClick);
+
+
 
 //====================================================================================================
-    $("#cookie").click(function(){
+    $("#cookie").click(function()
+    {
+
+      $("#cookies").text(cookies);
+      $("#cookiesPerSecond").text(cookiesPerSecond);
+      $("#cookiesPerClick").text(cookiesPerClick);
       cookies = cookies + cookiesPerClick;
-      $("#count").text(cookies);
+
+
     });
 
 //====================================================================================================
 
-	 $("#save").click(function(){
-	    alert("hi");
-        $.post("save",count);
+	 $("#save").click(function()
+   {
+
+        cookiesPerClick = cookiesPerClick + 1;
+        $("#cookies").text(cookies);
+        $("#cookiesPerSecond").text(cookiesPerSecond);
+        $("#cookiesPerClick").text(cookiesPerClick);
+
   });
 //====================================================================================================
 
+  $("#cursor").click(function(){
+    $("#cookies").text(cookies);
+    $("#cookiesPerSecond").text(cookiesPerSecond);
+    $("#cookiesPerClick").text(cookiesPerClick);
+      cookiesPerSecond = cookiesPerSecond + 1;
 
-
+  });
 
 
 });
