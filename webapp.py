af1a195614c2f031e9d88e9e911e7de80e23323d
benjaminelizalde <42652971+benjaminelizalde@users.noreply.github.com>
2019-06-04 10:33:54 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -93,7 +93,7 @@ def render_save():
 @app.route('/login')
 def login():
 
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='http')) #callback URL must match the pre-configured callback URL
+    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
 
 
 @app.route('/logout')
